becaf690a1f5606de05119c989ea8b468db85998
Ryan Phillips <ryanmphill@gmail.com>
2023-09-08 14:03:51 -0500

    Added method for updating study

--- wilderwatch_api/serializers/study_serializer.py
+++ wilderwatch_api/serializers/study_serializer.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
-from wilderwatch_api.models import Study
+from wilderwatch_api.models import Study, WilderUser
 from wilderwatch_api.serializers.wilder_user_serializer import WilderUserSerializer
 from wilderwatch_api.serializers.study_type_serializer import StudyTypeSerializer
 from wilderwatch_api.serializers.region_serializer import RegionSerializer
 from wilderwatch_api.serializers.observation_serializer import ObservationSerializer
+from wilderwatch_api.serializers.user_serializer import UserSerializer
+
 
 class StudySerializer(serializers.ModelSerializer):
 
@@ -11,6 +13,6 @@ class StudySerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Study
-        fields = ('id', 'author', 'subject', 'summary', 'details', 'start_date', 'end_date',
+        fields = ('id', 'title', 'author', 'subject', 'summary', 'details', 'start_date', 'end_date',
                   'is_complete', 'study_type', 'region', 'image_url', 'observations')
         depth = 1
--- wilderwatch_api/views/study_views.py
+++ wilderwatch_api/views/study_views.py
@@ -62,3 +62,41 @@ class StudyView(ViewSet):
             return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
         except KeyError as ex:
             return Response({'message': f"{ex.args[0]} is required"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk):
+        """Update a study"""
+
+        try:
+            region = Region.objects.get(pk=request.data['regionId'])
+            study_type_instance = StudyType.objects.get(pk=request.data['studyTypeId'])
+            image_url = "https://images.unsplash.com/photo-1619468129361-605ebea04b44?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80"
+
+            if len(request.data['imageUrl']) > 0:
+                image_url = request.data['imageUrl']
+            end_date = request.data['endDate']
+            if len(end_date) == 0:
+                end_date = None
+            
+            current_wilder_user = WilderUser.objects.get(user=request.auth.user)
+
+            study = Study.objects.get(
+                pk=pk, author=current_wilder_user)
+            
+            study.title=request.data['title']
+            study.subject=request.data['subject']
+            study.summary=request.data['summary']
+            study.details=request.data['details']
+            study.start_date=request.data['startDate']
+            study.end_date=end_date
+            study.study_type=study_type_instance
+            study.region=region
+            study.image_url=image_url
+
+            study.save()
+            return Response(None, status=status.HTTP_204_NO_CONTENT)
+        except ValidationError as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+        except Study.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except KeyError as ex:
+            return Response({'message': f"{ex.args[0]} is required"}, status=status.HTTP_400_BAD_REQUEST)
