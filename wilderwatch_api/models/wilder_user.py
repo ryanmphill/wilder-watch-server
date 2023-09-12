5f267bc6c63f74707170a3af2dec0831d56ca613
Ryan Phillips <ryanmphill@gmail.com>
2023-09-12 10:37:18 -0500

    Added localhost:5173 to cors whitelist. Added default profile image to users. Modified response data for register endpoint

--- wilderwatch_api/models/wilder_user.py
+++ wilderwatch_api/models/wilder_user.py
@@ -6,7 +6,7 @@ class WilderUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=1000, blank=True, null=True)
     flair = models.CharField(max_length=150, blank=True, null=True)
-    image_url = models.CharField(max_length=600, blank=True, null=True)
+    image_url = models.CharField(max_length=600, blank=True, null=True, default="https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg")
     is_researcher = models.BooleanField(default=False)
 
     @property
